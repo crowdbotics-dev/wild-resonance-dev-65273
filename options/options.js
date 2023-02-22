4ddd2dd6848b1ca0f81c1de1a951115b1bd10ea2
Crowdbotics <engineering@crowdbotics.com>
2023-02-22 06:18:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wild-resonance-dev-65273",
+  url: "https://wild-resonance-dev-65273.botics.co",
+  api: "https://wild-resonance-dev-65273.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
